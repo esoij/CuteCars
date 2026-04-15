5f60ad6f4578f512787bfee33b6d2e5085802d0c
lowercasebtw <jejojora2@gmail.com>
2026-04-15 17:45:14 -0500

    Add CuteCars#identifier

--- src/main/java/io/github/cutecarsmc/CuteCars.java
+++ src/main/java/io/github/cutecarsmc/CuteCars.java
@@ -1,7 +1,13 @@
 package io.github.cutecarsmc;
 
+import net.minecraft.resources.Identifier;
+
 public final class CuteCars {
     public static final String MOD_ID = "@MODID@";
     public static final String VERSION = "@VERSION@";
     public static final String COMMIT_HASH = "@COMMIT_HASH@";
+
+    public static Identifier identifier(final String path) {
+        return Identifier.fromNamespaceAndPath(MOD_ID, path);
+    }
 }
